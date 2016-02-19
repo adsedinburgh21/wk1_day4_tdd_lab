6a0efec7601818752834394986c9ec8c2b9c7342
Adam Reid <adamreid21@hotmail.co.uk>
2016-02-19 18:23:03 +0000

    TDD Lab finished

--- friends.rb
+++ friends.rb
@@ -10,73 +10,85 @@
 # INSANE
 # Find the people who have the same favourite tv show
 
-#   #1
-# def tv_show(person)
-#   tv_show = person[:favourites][:tv_show]
-#   return tv_show
-# end
+  #1
+def tv_show(person)
+  tv_show = person[:favourites][:tv_show]
+  return tv_show
+end
 
 
-#   #2
-# def favourite_food(person, food)
-#   array = person[:favourites][:things_to_eat]
-#   for i in array
-#     if i == food
-#       return true
-#     end
-#   end
-#   return false
-# end
+  #2
+def favourite_food(person, food)
+  array = person[:favourites][:things_to_eat]
+  for i in array
+    if i == food
+      return true
+    end
+  end
+  return false
+end
 
 
-#   #3
-# def add_friend(person, *new_friend)
-#   array = person[:friends]
-#   final_array = array.push(new_friend)
-#   return final_array.flatten
-# end
+  #3
+def add_friend(person, *new_friend)
+  array = person[:friends]
+  final_array = array.push(new_friend)
+  return final_array.flatten
+end
 
 
-#   #4
-# def remove_friend(person, new_friend)
-#   array = person[:friends]
-#     for friend in array
-#       if friend == new_friend
-#           #wanting to leave possiblity of deleting multiple friends
-#         array.delete(new_friend)
-#         return array
-#       end
-#     end
-#     return false
-# end
+  #4
+def remove_friend(person, new_friend)
+  array = person[:friends]
+    for friend in array
+      if friend == new_friend
+          #wanting to leave possiblity of deleting multiple friends
+        array.delete(new_friend)
+        return array
+      end
+    end
+    return false
+end
 
 
-#   #5
-# def add_money(*person, key)
-#   counter = 0
-#   for i in person
-#     counter += i[key]
-#   end
-#   return counter
-# end
+  #5
+def add_money(*person, key)
+  counter = 0
+  for i in person
+    counter += i[key]
+  end
+  return counter
+end
 
 
-#   #6
-# def loan_money(person_1, person_2, amount)
-#   person_1[:monies] -= amount
-#   person_2[:monies] += amount
-# end
+  #6
+def loan_money(person_1, person_2, amount)
+  person_1[:monies] -= amount
+  person_2[:monies] += amount
+end
 
 
-#   #7
-# def everyones_fav_food(*person)
-#   fav_food_array = []
-#   for i in person
-#     fav_food_array.push( i[:favourites][:things_to_eat] )
-#   end
-#   return fav_food_array.flatten
-# end
-  
+  #7
+def everyones_fav_food(*person)
+  fav_food_array = []
+  for i in person
+    fav_food_array.push( i[:favourites][:things_to_eat] )
+  end
+  return fav_food_array.flatten
+end
+
+
+#  #8
+def no_friends(*person)
+  loaners= []
+  for i in person
+    if i[:friends] == []
+      loaners.push(i[:name])
+    end
+  end
+  return loaners
+end
+
 
 
 
--- specs/friends_spec.rb
+++ specs/friends_spec.rb
@@ -64,71 +64,75 @@ class TestFriends < MiniTest::Test
 
   end
 
-  # def test_name
-  #   assert_equal("Marc", @person5[:name])
-  # end
+  def test_name
+    assert_equal("Marc", @person5[:name])
+  end
   
 
-  #   #1
-  # def test_tv_show()
-  #   result = tv_show(@person2)
-  #   assert_equal("Scrubs", result)
-  # end
+    #1
+  def test_tv_show()
+    result = tv_show(@person2)
+    assert_equal("Scrubs", result)
+  end
 
 
-  #   #2
-  # def test_favourite_food()
-  #   result = favourite_food(@person3, "pizza")
-  #   assert_equal(false, result)
-  # end
+    #2
+  def test_favourite_food()
+    result = favourite_food(@person3, "pizza")
+    assert_equal(false, result)
+  end
 
 
-  #   #3
-  # def test_add_friend()
-  #   result = add_friend(@person4, "Garry", "John", "Bob", "Kerry")
-  #   array= ["Rick", "Jay", "Marc", "Garry","John", "Bob", "Kerry" ]
-  #   assert_equal(array, result)
-  # end
+    #3
+  def test_add_friend()
+    result = add_friend(@person4, "Garry", "John", "Bob", "Kerry")
+    array= ["Rick", "Jay", "Marc", "Garry","John", "Bob", "Kerry" ]
+    assert_equal(array, result)
+  end
 
 
-  #   #4
-  # def test_remove_friend()
-  #   result = remove_friend(@person1, "Keith")
-  #   array = ["Jay", "Marc", "Val"]
-  #   assert_equal(array, result)
-  # end
+    #4
+  def test_remove_friend()
+    result = remove_friend(@person1, "Keith")
+    array = ["Jay", "Marc", "Val"]
+    assert_equal(array, result)
+  end
 
 
-  #   #5
-  # def test_add_money()
-  #   result = add_money(@person1, @person2, @person3, @person4, @person5, :monies)
-  #   assert_equal(143, result)
-  # end
+    #5
+  def test_add_money()
+    result = add_money(@person1, @person2, @person3, @person4, @person5, :monies)
+    assert_equal(143, result)
+  end
 
 
-  #  #6
-  # def test_loan_money    
-  #   giver = @person1
-  #   receiver = @person2
-  #   money = 1
+   #6
+  def test_loan_money    
+    giver = @person1
+    receiver = @person2
+    money = 1
 
-  #   loan_money(giver, receiver, money)
+    loan_money(giver, receiver, money)
 
-  #   assert_equal( 0, giver[:monies])
-  #   assert_equal( 3, receiver[:monies])
+    assert_equal( 0, giver[:monies])
+    assert_equal( 3, receiver[:monies])
 
-    # # puts "#{giver[:name]}'s balance is now £#{giver[:monies]-money} and {receiver[:name]}'s balance is now £#{receiver[:monies]+money}"
-     # # This puts is not needed, it just puts out the sentence when the test is run.
-  # end
+    # puts "#{giver[:name]}'s balance is now £#{giver[:monies]-money} and {receiver[:name]}'s balance is now £#{receiver[:monies]+money}"
+     # This puts is not needed, it just puts out the sentence when the test is run.
+  end
 
 
-  #  #7
+   #7
   def test_everyones_fav_food
     result= everyones_fav_food(@person1, @person2, @person3, @person4, @person5)
     assert_equal( ["charcuterie", "soup", "bread", "ratatouille", "stew", "spaghetti", "spinach"], result)
   end
 
 
-
+  # #8
+def test_no_friends()
+  result= no_friends(@person1, @person2, @person3, @person4, @person5)
+  assert_equal( ["Marc"], result)
+end
 
 end
